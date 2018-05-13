1cac691b3a79c4fa8dad35becb2596c59668446a
andreev <vladislavandand@gmail.com>
2018-05-13 14:16:19 +0300

    fix: add to slugify russian symbols

--- themes/helper_slugify.go
+++ themes/helper_slugify.go
@@ -6,6 +6,6 @@ import (
 )
 
 func helperSlugify(label string) string {
-	re := regexp.MustCompile("[^a-z0-9]+")
+	re := regexp.MustCompile("[^a-z0-9а-я]+")
 	return strings.Trim(re.ReplaceAllString(strings.ToLower(label), "-"), "-")
 }
